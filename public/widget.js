83343bee35a6ff80a89cd7589bc66ab81711316b
laith-dotcom <laith@albasha.store>
2025-12-10 23:31:52 -0500

    Update widget.js

--- public/widget.js
+++ public/widget.js
@@ -1,6 +1,6 @@
 (() => {
   // --- CONFIG ---
-  const SERVER_URL = "https://albasha-ai-server.vercel.app"; // Vercel app
+  const SERVER_URL = "https://n8n.srv1182142.hstgr.cloud/webhook/Albasha-Chat"; // Vercel app
   const PRIMARY = "#A4472E"; // Albasha Red
   const ICON = "💬";
 
